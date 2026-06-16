493c58c724a0c3528bc1ab21b11e22eedfba6acf
Anshu <anshpoddar89@gmail.com>
2026-06-16 14:01:16 +0530

    Modifying a table

--- QUERY1.sql
+++ QUERY1.sql
@@ -9,3 +9,14 @@ create table students(
     admission_date Date
 );
 SELECT * FROM STUDENTS; 
+Show tables;  
+describe students; 
+rename table students to studens1;
+use anshu;
+show tables;
+describe studens1;
+rename table studens1 to students;
+alter table students rename column admission_date to adm_dt;
+describe students;
+alter table students drop column adm_dt;
+alter table students add column is_passed Bool deafult True; 
