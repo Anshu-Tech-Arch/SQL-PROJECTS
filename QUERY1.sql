6625e668a609dcb8457f655b27f0621ad2471040
Anshu <anshpoddar89@gmail.com>
2026-06-20 13:00:31 +0530

    Date & Time Completed

--- QUERY1.sql
+++ QUERY1.sql
@@ -1,1 +1,10 @@
 select current_date;
+select current_time;
+select current_timestamp;
+select localtime;
+select localtimestamp;
+use schooldb;
+alter table student add column date_joined datetime default (now());
+select * from student;
+insert into student (id, age, date_joined) 
+values(23, 67, now());
