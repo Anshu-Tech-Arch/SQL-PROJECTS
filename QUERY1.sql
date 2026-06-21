041f2bb76f17b99952369bc029f4ff4d1804f206
Anshu <anshpoddar89@gmail.com>
2026-06-21 19:40:35 +0530

    Deep Dive into Constrainsts

--- QUERY1.sql
+++ QUERY1.sql
@@ -1,10 +1,8 @@
-select current_date;
-select current_time;
-select current_timestamp;
-select localtime;
-select localtimestamp;
 use schooldb;
-alter table student add column date_joined datetime default (now());
-select * from student;
-insert into student (id, age, date_joined) 
-values(23, 67, now());
+create table accounts(
+	id int unique,
+    balance Decimal(10,2) check (balance>=0)
+);
+select * from accounts;
+
+insert into accounts(id, balance) values (2, 55600.56);
